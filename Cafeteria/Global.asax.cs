12ee37b8afa779eef3a2fa50ee07aca5a5b245c5
Fabian CN <Fabian CN@FABIANPC>
2025-07-31 16:44:16 -0600

    Cuentas

--- Cafeteria/Global.asax.cs
+++ Cafeteria/Global.asax.cs
@@ -1,3 +1,6 @@
+using Cafeteria.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +19,48 @@ namespace Cafeteria
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            CreateRoles();
+        }
+        private void CreateRoles()
+        {
+            var context = new ApplicationDbContext();
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            if (!roleManager.RoleExists("Administrador"))
+            {
+                var role = new IdentityRole("Administrador");
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Cajero"))
+            {
+                var role = new IdentityRole("Cajero");
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Cocinero"))
+            {
+                var role = new IdentityRole("Cocinero");
+                roleManager.Create(role);
+            }
+
+            var admin = userManager.FindByEmail("administrador@gmail.com");
+            if (admin != null && !userManager.IsInRole(admin.Id, "Administrador"))
+            {
+                userManager.AddToRole(admin.Id, "Administrador");
+            }
+            var cajero = userManager.FindByEmail("cajero@gmail.com");
+            if (cajero != null && !userManager.IsInRole(cajero.Id, "Cajero"))
+            {
+                userManager.AddToRole(cajero.Id, "Cajero");
+            }
+            var cocinero = userManager.FindByEmail("cocinero@gmail.com");
+            if (cocinero != null && !userManager.IsInRole(cocinero.Id, "Cocinero"))
+            {
+                userManager.AddToRole(cocinero.Id, "Cocinero");
+            }
+
+
         }
     }
 }
